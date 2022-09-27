efc47a945b8cb88b5ae51ac9a5eca57cf9f6fd3a
vitorcarazzatto <vitor_carazzatto@hotmail.com>
2022-09-27 15:27:42 -0300

    Fluent API Configuration + First Migration

--- Data/PimContext.cs
+++ Data/PimContext.cs
@@ -17,6 +17,52 @@ namespace PimAplication.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            modelBuilder.Entity<Endereco>(entity =>
+            {
+                entity.ToTable("ENDERECO").HasKey(x => x.Id);
+                entity.Property(x => x.Id).HasColumnType("integer");
+                entity.Property(x => x.Logradouro).HasColumnType("varchar(256)");
+                entity.Property(x => x.Numero).HasColumnType("integer");
+                entity.Property(x => x.Cep).HasColumnType("integer");
+                entity.Property(x => x.Bairro).HasColumnType("varchar(50)");
+                entity.Property(x => x.Cidade).HasColumnType("varchar(30)");
+                entity.Property(x => x.Estado).HasColumnType("varchar(20)");
+            });
+
+            modelBuilder.Entity<Pessoa>(entity =>
+            {
+                entity.ToTable("PESSOA").HasKey(x => x.Id);
+                entity.Property(x => x.Id).HasColumnType("integer");
+                entity.Property(x => x.Nome).HasColumnType("varchar(256)");
+                entity.Property(x => x.Cpf).HasColumnType("bigint");
+                entity.Property(x => x.EnderecoId).HasColumnType("integer");
+            });
+
+            modelBuilder.Entity<PessoaTelefone>(entity =>
+            {
+                entity.ToTable("PESSOA_TELEFONE");
+                entity.Property(x => x.PessoaId).HasColumnName("ID_PESSOA").HasColumnType("integer");
+                entity.Property(x => x.TelefoneId).HasColumnName("ID_TELEFONE").HasColumnType("integer");
+                entity.HasKey(x => new { x.PessoaId, x.TelefoneId });
+            });
+
+            modelBuilder.Entity<Telefone>(entity =>
+            {
+                entity.ToTable("TELEFONE").HasKey(x => x.Id);
+                entity.Property(x => x.Id).HasColumnType("integer");
+                entity.Property(x => x.Numero).HasColumnType("integer");
+                entity.Property(x => x.ddd).HasColumnType("integer");
+                entity.Property(x => x.TipoTelefoneId).HasColumnType("integer");
+            });
+
+            modelBuilder.Entity<TipoTelefone>(entity =>
+            {
+                entity.ToTable("TELEFONE_TIPO").HasKey(x => x.Id);
+                entity.Property(x => x.Id).HasColumnType("integer");
+                entity.Property(x => x.Tipo).HasColumnType("varchar(10)");
+            });
+
         }
     }
 }
--- Models/Entities/Pessoa.cs
+++ Models/Entities/Pessoa.cs
@@ -4,16 +4,14 @@ namespace PimAplication.Models.Entities
 {
     public class Pessoa
     {
+        public int Id { get; set; }
         public int PessoaId { get; set; }
         public string Nome { get; set; }
         public long Cpf { get; set; }
 
         //Relacionamento com a tabela Endereco
         public int EnderecoId { get; set; }
-        public Endereco Endereco { get; set; }
+        public Endereco ?Endereco { get; set; }
 
-        //Relacionamento com a tabela Telefone
-        public int TelefoneId { get; set; }
-        public Telefone Telefone { get; set; }
     }
 }
